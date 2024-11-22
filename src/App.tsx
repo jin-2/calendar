f3b306f4db44522dbacca73600800eca7453041f
sujin2 <jmakeaz@gmail.com>
2024-11-22 17:56:18 +0900

    feat: Set default options to react-query

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,14 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Recruit from "./pages/Recruit/Recruit.tsx";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: 1000 * 60 * 3,
+    },
+  },
+});
 
 function App() {
   return (
