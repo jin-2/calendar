958252b51aa7a8fb4307d613cbe69a72c0d9be32
sujin2 <jmakeaz@gmail.com>
2024-11-23 14:47:20 +0900

    feat: Move logic to the component that needs the data by re-rendering the response.

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,26 +1,31 @@
 "use client";
 import { useMemo } from "react";
 import styled from "@emotion/styled";
+import { useQuery } from "@tanstack/react-query";
+import { getRecruits } from "../../api/calendar.ts";
 import { RecruitData } from "../../types/recruit.ts";
 import { getMonthByWeek } from "../../utils/getMonthByWeek.ts";
 import { getArrayToMap } from "../../utils/getArrayToMap.ts";
 import { getCalendarData } from "../../utils/calendar.ts";
 import Day from "../Day/Day.tsx";
 
-interface CalendarProps {
-  recruitData: RecruitData[];
-}
+interface CalendarProps {}
 
-const Calendar = ({ recruitData }: CalendarProps) => {
+const Calendar = ({}: CalendarProps) => {
+  const { data } = useQuery({
+    queryKey: ["RECRUITS"],
+    queryFn: getRecruits,
+  });
+
   const days = getMonthByWeek(2024, 11);
 
   const recruitDataToMap = useMemo(() => {
-    return getArrayToMap<RecruitData>(recruitData ?? [], "id");
-  }, [recruitData]);
+    return getArrayToMap<RecruitData>(data ?? [], "id");
+  }, [data]);
 
   const recruitDataByCalendar = useMemo(() => {
-    return getCalendarData(recruitData ?? []);
-  }, [recruitData]);
+    return getCalendarData(data ?? []);
+  }, [data]);
 
   return (
     <StyledCalendar>
--- src/pages/Recruit/Recruit.tsx
+++ src/pages/Recruit/Recruit.tsx
@@ -1,25 +1,13 @@
 "use client";
 import styled from "@emotion/styled";
-import { useQuery } from "@tanstack/react-query";
-import { getFilters, getRecruits } from "../../api/calendar.ts";
 import Calendar from "../../components/Calendar/Calendar.tsx";
+import FilterDuties from "../../components/FilterDuties/FilterDuties.tsx";
 
-interface RecruitProps {}
-
-const Recruit = ({}: RecruitProps) => {
-  const recruitData = useQuery({
-    queryKey: ["RECRUITS"],
-    queryFn: getRecruits,
-  });
-
-  const filtersData = useQuery({
-    queryKey: ["DUTIES"],
-    queryFn: getFilters,
-  });
-
+const Recruit = () => {
   return (
     <StyledRecruit>
-      <Calendar recruitData={recruitData.data ?? []} />
+      <FilterDuties />
+      <Calendar />
     </StyledRecruit>
   );
 };
