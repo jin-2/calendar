3e03408454d17ec417775ea8e62532e74ad5a10a
sujin2 <jmakeaz@gmail.com>
2024-11-24 14:08:22 +0900

    feat: Add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Global } from "@emotion/react";
 import Recruit from "./pages/Recruit/Recruit.tsx";
+import globalStyles from "./styles/globalStyles.ts";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -14,6 +16,7 @@ const queryClient = new QueryClient({
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <Global styles={globalStyles} />
       <Recruit />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
