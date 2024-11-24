40690434e97e693925444e1154421d7744c0b7fb
sujin2 <jmakeaz@gmail.com>
2024-11-24 16:39:55 +0900

    feat: Add recruitment detail style

--- src/components/JobDetail/JobDetail.tsx
+++ src/components/JobDetail/JobDetail.tsx
@@ -48,32 +48,43 @@ const JobDetail = ({
     <StyledJobDetail>
       <div>
         {prevContent && (
-          <button type="button" onClick={() => handleClickNext(prevContent.id)}>
-            이전: {prevContent.company_name}
+          <button
+            type="button"
+            className="detail-navigation prev"
+            onClick={() => handleClickNext(prevContent.id)}
+          >
+            &lt; {prevContent.company_name}
           </button>
         )}
         {nextContent && (
-          <button type="button" onClick={() => handleClickNext(nextContent.id)}>
-            다음: {nextContent.company_name}
+          <button
+            type="button"
+            className="detail-navigation next"
+            onClick={() => handleClickNext(nextContent.id)}
+          >
+            {nextContent.company_name} &gt;
           </button>
         )}
         {currentContent && (
-          <div>
-            <h3>{currentContent.company_name}</h3>
-            <h4>{currentContent.title}</h4>
-            <p>
-              {currentContent.duty_ids
-                .map((id) => dutiesMap.get(id)?.name)
-                .join(", ")}
-            </p>
-            <p>
-              {formatDate(new Date(currentContent.start_time), TIME_FORMAT)} ~{" "}
-              {formatDate(new Date(currentContent.end_time), TIME_FORMAT)}
-            </p>
+          <>
+            <div className="content-text-wrap">
+              <h3 className="company-name">{currentContent.company_name}</h3>
+              <h4 className="recruitment-title">{currentContent.title}</h4>
+              <p>
+                -{" "}
+                {currentContent.duty_ids
+                  .map((id) => dutiesMap.get(id)?.name)
+                  .join(", ")}
+              </p>
+              <p>
+                - {formatDate(new Date(currentContent.start_time), TIME_FORMAT)}{" "}
+                ~ {formatDate(new Date(currentContent.end_time), TIME_FORMAT)}
+              </p>
+            </div>
             <div className="content-image-wrap">
               <img src={currentContent.image_url} alt="" />
             </div>
-          </div>
+          </>
         )}
       </div>
     </StyledJobDetail>
@@ -83,11 +94,48 @@ const JobDetail = ({
 export default JobDetail;
 
 const StyledJobDetail = styled.div`
-  padding: 20px;
+  padding: 40px 0;
 
   .content-image-wrap {
+    margin-top: 40px;
+
     img {
       width: 100%;
     }
+  }
+
+  .content-text-wrap {
+    padding-left: 20px;
+    padding-right: 20px;
+
+    p + p {
+      margin-top: 4px;
+    }
+  }
+
+  .recruitment-title {
+    margin-top: 4px;
+    margin-bottom: 10px;
+    font-size: 24px;
+  }
+
+  .detail-navigation {
+    position: fixed;
+    top: 45%;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    color: #fff;
+    font-size: 18px;
+    font-weight: bold;
+  }
+
+  .prev {
+    left: 10px;
+    text-align: left;
+  }
+
+  .next {
+    right: 10px;
+    text-align: right;
   }
 `;
