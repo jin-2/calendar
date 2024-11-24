4b336356034133a5b72272fa421db32a73ba0f69
sujin2 <jmakeaz@gmail.com>
2024-11-24 23:32:51 +0900

    chore: Remove devtool

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Global } from "@emotion/react";
 import Recruit from "./pages/Recruit/Recruit.tsx";
 import globalStyles from "./styles/globalStyles.ts";
@@ -18,7 +17,6 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <Global styles={globalStyles} />
       <Recruit />
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
